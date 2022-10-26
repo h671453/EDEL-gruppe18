3512255e4445cb4c561fbdbb7ab0434102d0728f
h671453 <ar-seln@online.no>
2022-10-26 12:01:30 +0200

    button

--- JS/Medlemskap.js
+++ JS/Medlemskap.js
@@ -1,8 +1,10 @@
 //let username = window.prompt("Name");
 let username;
 let userEmail;
-document.getElementById("usernameButton").onclick = function(){
+let password;
+document.getElementById("userButton").onclick = function(){
     username = document.getElementById("usernameInput").value;
     userEmail= document.getElementById("userEmailInput").value;
-    console.log(username + userEmail);
+    password = document.getElementById("userPassword").value;
+    console.log(username + userEmail + password);
 }
