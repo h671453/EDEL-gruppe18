16a57d8c77c75467cf7d0f0d7ad54733ccec357a
h671453 <ar-seln@online.no>
2022-10-26 14:07:50 +0200

    color og js

--- JS/Medlemskap.js
+++ JS/Medlemskap.js
@@ -1,10 +1,27 @@
 //let username = window.prompt("Name");
-let username;
+/*let username;
 let userEmail;
 let password;
 document.getElementById("userButton").onclick = function(){
     username = document.getElementById("usernameInput").value;
     userEmail= document.getElementById("userEmailInput").value;
     password = document.getElementById("userPassword").value;
+    pas
     console.log(username + userEmail + password);
+}*/
+
+const username = document.getElementById("usernameInput");
+const email = document.getElementById("userEmailInput");
+const password = document.getElementById("userPassword");
+const password2 = document.getElementById("userpassword2");
+
+function checkinputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(password2Value !== passwordValue){
+        setErrorfor(password2,'Passwords does not match')
+    }
 }
