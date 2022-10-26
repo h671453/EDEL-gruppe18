7accdc1d10a03364568ef7337f7f400568abd0a9
h671453 <ar-seln@online.no>
2022-10-26 14:48:12 +0200

    redigering js

--- JS/Medlemskap.js
+++ JS/Medlemskap.js
@@ -6,6 +6,5 @@ document.getElementById("userButton").onclick = function(){
     username = document.getElementById("usernameInput").value;
     userEmail= document.getElementById("userEmailInput").value;
     password = document.getElementById("userPassword").value;
-    pas
     console.log(username,' ', userEmail,' ', password);
 }
