a7905081b7ffaad28a232abdfb260ffcffd82325
h671453 <ar-seln@online.no>
2022-10-26 11:58:15 +0200

    java update

--- JS/Medlemskap.js
+++ JS/Medlemskap.js
@@ -1,6 +1,8 @@
 //let username = window.prompt("Name");
 let username;
+let userEmail;
 document.getElementById("usernameButton").onclick = function(){
-    username=document.getElementById("usernameInput").value;
-    console.log(username);
+    username = document.getElementById("usernameInput").value;
+    userEmail= document.getElementById("userEmailInput").value;
+    console.log(username + userEmail);
 }
